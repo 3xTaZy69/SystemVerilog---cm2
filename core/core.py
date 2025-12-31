0af5040666f0ea78b37b8da62f6bc660fc01fd89
TerryTheKingFan <108461715+3xTaZy69@users.noreply.github.com>
2025-12-31 19:18:35 +0200

    Update core.py

--- core/core.py
+++ core/core.py
@@ -4,6 +4,9 @@ scene_blocks = {}
 scene_connections = []
 d_ff_count = 0
 class block:
+    """
+    block
+    """
     def __init__(self, index: int, x: int, y: int, z: int, params: list[int], name: str):
         self.index = str(index)
         self.x = str(x)
@@ -31,6 +34,7 @@ class PD_ff:
     """
     Orientation counter-clockwise
     start_pos - x, y, z
+    positive edge d flip flop
     """
     def __init__(self, orientation: int, start_pos: list[int], input_block: block):
         global d_ff_count
@@ -81,6 +85,7 @@ class ND_ff:
     """
     Orientation counter-clockwise
     start_pos - x, y, z
+    negative edge d flip flop
     """
     def __init__(self, orientation: int, start_pos: list[int], input_block: block):
         global d_ff_count
@@ -150,6 +155,9 @@ class ND_ff:
             scene_connections.append(connection)
 
 class scene_connection:
+    """
+    connection
+    """
     def __init__(self, src: str, dst: str):
         self.src = src
         self.dst = dst
@@ -159,6 +167,9 @@ output_code_blocks = ""
 output_code_connections = ""
 
 def build_scene():
+    """
+    builds scene
+    """
     global scene_blocks, scene_connections, output_code_connections, output_code_blocks, end_time
 
     for connection in scene_connections:
@@ -177,6 +188,9 @@ def build_scene():
         pyperclip.copy(output_code_blocks[:-1]+"?"+output_code_connections[:-1]+"??")
 
 class always_relay_on:
+    """
+    makes a d flip flop relay on some block like clock etc.
+    """
     def __init__(self, block_to_relay_on, d_ff, output_block):
         if d_ff.type == "posedge":
             scene_connections.append(scene_connection(block_to_relay_on.world_name, f"d_ff_and{d_ff.index}"))
@@ -187,6 +201,9 @@ class always_relay_on:
 
 and_gate_count = 0
 def and_gate(input1, input2, position: list[int]):
+    """
+    and gate
+    """
     global scene_connections, scene_blocks, and_gate_count
 
     gate = block(1, position[0], position[1], position[2], [], f"and_gate{and_gate_count}")
@@ -198,6 +215,9 @@ def and_gate(input1, input2, position: list[int]):
 
 not_gate_count = 0
 def not_gate(input, position: list[int]):
+    """
+    not gate
+    """
     global scene_connections, scene_blocks, not_gate_count
 
     gate = block(0, position[0], position[1], position[2], [], f"not_gate{not_gate_count}")
@@ -208,6 +228,9 @@ def not_gate(input, position: list[int]):
 
 or_gate_count = 0
 def or_gate(input1, input2, position: list[int]):
+    """
+    or gate
+    """
     global scene_connections, scene_blocks, or_gate_count
 
     gate = block(2, position[0], position[1], position[2], [], f"or_gate{or_gate_count}")
@@ -219,6 +242,9 @@ def or_gate(input1, input2, position: list[int]):
 
 xor_gate_count = 0
 def xor_gate(input1, input2, position: list[int]):
+    """
+    xor gate
+    """
     global scene_connections, scene_blocks, xor_gate_count
 
     gate = block(3, position[0], position[1], position[2], [], f"xor_gate{xor_gate_count}")
@@ -230,6 +256,9 @@ def xor_gate(input1, input2, position: list[int]):
 
 nor_gate_count = 0
 def nor_gate(input1, input2, position: list[int]):
+    """
+    nor gate
+    """
     global scene_connections, scene_blocks, nor_gate_count
 
     gate = block(0, position[0], position[1], position[2], [], f"nor_gate{nor_gate_count}")
@@ -241,14 +270,17 @@ def nor_gate(input1, input2, position: list[int]):
 
 
 logics = 0
-def logic(start_pos: list[int], end_pos: list[int], width: int):
+def logic(start_pos: list[int], end_pos: list[int], width: int, block_type: int):
+    """
+    generation of busses
+    """
     global logics
     step = [(end_pos[i] - start_pos[i]) // max(1, width-1) for i in range(3)]
     blocks = []
 
     for b in range(width):
         pos = [start_pos[i] + step[i]*b for i in range(3)]
-        blocks.append(block(15, pos[0], pos[1], pos[2], [], f"logic{b}_{logics}"))
+        blocks.append(block(block_type, pos[0], pos[1], pos[2], [], f"logic{b}_{logics}"))
 
     logics += 1
     return blocks
@@ -257,6 +289,7 @@ r_adders = 0
 def ripple_adder(start_pos: list[int], width: int, type: str):
     """
     type: add for adding, sub for subtracting 
+    dynamic generation of ripple adders
     """
     global r_adders, scene_connections, scene_blocks
 
@@ -308,7 +341,103 @@ def ripple_adder(start_pos: list[int], width: int, type: str):
 
     return [b for b in blocks if (b.world_name.startswith("r_adder_A_") or b.world_name.startswith("r_adder_B_") or b.world_name.startswith("r_adder_C_"))]
 
-def mux(inputs: list[block], output: list[block], select: list[block], position: list[int]):
+mux_i_count = 0
+def mux_i(inputs: list, select: list[block], position: list[int], input_width: int):
+    inputs = [[inputs[0], inputs[1]]] + [[x] for x in inputs[2:]]
+    """
+    dynamic generation of muxes with inputs for selection
+    """
+    global mux_i_count, logics
     sel_len = len(inputs)
-    
+    mux_count = sel_len - 1 
+    blocks = []
+    connections = []
+    y = position[1]
+    z = position[2]
+    output_logic = logic([position[0],0,-4], [position[0]+input_width,0,-4], input_width, 15)
+    for m in range(mux_count):
+        x = position[0]+input_width*m
+        blocks.append(block(15, x, y, z, [], f"mux_i_sel_{m}_{mux_i_count}"))
+        blocks.append(block(0, x+1, y, z, [], f"mux_i_!sel_{m}_{mux_i_count}"))
+        [blocks.append(b) for b in logic([x,y,z-1],[x+input_width,y,z-1],input_width,1)]
+        [blocks.append(b) for b in logic([x,y,z-2],[x+input_width,y,z-2],input_width,1)]
+        [blocks.append(b) for b in logic([x,y,z-3],[x+input_width,y,z-3],input_width,15)]
+
+        connections.append(scene_connection(f"mux_i_sel_{m}_{mux_i_count}", f"mux_i_!sel_{m}_{mux_i_count}"))
+        connections.append(scene_connection(select[m].world_name, f"mux_I_sel_{m}_{mux_i_count}"))
+        if m == 0:
+            for c in range(input_width):
+                connections.append(scene_connection(f"mux_i_sel_{m}_{mux_i_count}", f"logic{c}_{logics-2}"))
+                connections.append(scene_connection(f"mux_i_!sel_{m}_{mux_i_count}", f"logic{c}_{logics-1}"))
+                connections.append(scene_connection(f"logic{c}_{logics-2}", f"logic{c}_{logics}"))
+                connections.append(scene_connection(f"logic{c}_{logics-1}", f"logic{c}_{logics}"))
+                connections.append(scene_connection(inputs[m][0][c].world_name, f"logic{c}_{logics-2}"))
+                connections.append(scene_connection(inputs[m][1][c].world_name, f"logic{c}_{logics-1}"))
+                connections.append(scene_connection(f"logic{c}_{logics}", output_logic[c].world_name))
+        else:
+            for c in range(input_width):
+                connections.append(scene_connection(f"mux_i_sel_{m}_{mux_i_count}", f"logic{c}_{logics-2}"))
+                connections.append(scene_connection(f"mux_i_!sel_{m}_{mux_i_count}", f"logic{c}_{logics-1}"))
+                connections.append(scene_connection(f"logic{c}_{logics-2}", f"logic{c}_{logics}"))
+                connections.append(scene_connection(f"logic{c}_{logics-1}", f"logic{c}_{logics}"))
+                connections.append(scene_connection(inputs[m][0][c].world_name, f"logic{c}_{logics-2}"))
+                connections.append(scene_connection(f"logic{c}_{logics-3}", f"logic{c}_{logics-1}"))
+                connections.append(scene_connection(f"logic{c}_{logics}", output_logic[c].world_name))
+        return output_logic
+
+mux_c_count = 0
+def mux_c(inputs: list, output: list[block], select: list[block], position: list[int], input_width: int):
+    inputs = [[inputs[0], inputs[1]]] + [[x] for x in inputs[2:]]
+    """
+    dynamic generation of muxes with const inputs for selection
+    """
+    global mux_c_count, logics
+    sel_len = len(inputs)
+    mux_count = sel_len - 1 
+    blocks = []
+    connections = []
+    y = position[1]
+    z = position[2]
+
+    for m in range(mux_count):
+        x = position[0]+input_width*m
+        output_logic = logic([position[0],0,-4], [position[0]+input_width,0,-4], input_width, 15)
+        blocks.append(block(15, x, y, z, [], f"mux_c_sel_{m}_{mux_c_count}"))
+        blocks.append(block(0, x+1, y, z, [], f"mux_c_!sel_{m}_{mux_c_count}"))
+        if m == 0:
+            for index, value in enumerate(inputs[m][0]):
+                if value == "0":
+                    blocks.append(block(15, x+index, 0, z-1, [], "mux_c_0_{m}_{mux_c_count}"))
+                elif value == "1":
+                    blocks.append(block(0, x+index, 0, z-1, [], "mux_c_1_{m}_{mux_c_count}"))
+            [blocks.append(b) for b in logic([x,y,z-2],[x+input_width,y,z-2],input_width,1)]
+            for index, value in enumerate(inputs[m][1]):
+                if value == "0":
+                    blocks.append(block(15, x+index, 0, z-3, [], "mux_c_0_{m}_{mux_c_count}"))
+                elif value == "1":
+                    blocks.append(block(0, x+index, 0, z-3, [], "mux_c_1_{m}_{mux_c_count}"))
+            [blocks.append(b) for b in logic([x,y,z-4],[x+input_width,y,z-4],input_width,1)]
+        else:
+            for index, value in enumerate(inputs[m][0]):
+                if value == "0":
+                    blocks.append(block(15, x+index, 0, z-1, [], "mux_c_0_{m}_{mux_c_count}"))
+                elif value == "1":
+                    blocks.append(block(0, x+index, 0, z-1, [], "mux_c_1_{m}_{mux_c_count}"))
+            [blocks.append(b) for b in logic([x,y,z-2],[x+input_width,y,z-2],input_width,1)]
+            [blocks.append(b) for b in logic([x,y,z-3],[x+input_width,y,z-3],input_width,1)]
+
+        
+
+        
+
+        scene_connections.append(scene_connection(f"mux_c_sel_{m}_{mux_c_count}", f"mux_c_!sel_{m}_{mux_c_count}"))
+        scene_connections.append(scene_connection(select[m].world_name, f"mux_c_sel_{m}_{mux_c_count}"))
+        if m == 0:
+            for c in range(input_width):
+                scene_connections.append(scene_connection())
+        else:
+            for c in range(input_width):
+                pass
+
+
 build_scene()
